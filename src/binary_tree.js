a42b94dd597075e0a3ab84ea82b4ca55ebbe3067
Paulo Canedo <paulocanedo@gmail.com>
2015-05-03 13:50:00 -0300

    now binary tree working insert, navigating and size

--- src/binary_tree.js
+++ src/binary_tree.js
@@ -9,6 +9,8 @@
 *** posOrder:  iterate pos order
 **/
 
+var nodeBuilder = require('./node');
+
 var binaryTree = (function() {
 	return {
 		create: BinaryTree
@@ -19,104 +21,84 @@ function BinaryTree() {
 	var size = 0;
 	var root = null;
 
-	var createNode = function(element) {
-		return {
-			element: element,
-			left:    null,
-			right:   null
-		};
-	};
-
-	var insertNode = function(node, lastNode) {
+	var insertNode = function(lastNode, newNode) {
 		if(lastNode !== null) {
-			if(node.element < lastNode.element) {
-				lastNode.left = insertNode(node, lastNode.left);
+			if(newNode.element < lastNode.element) {
+				if(lastNode.left) {
+					insertNode(lastNode.left, newNode);
+				} else {
+					lastNode.left = newNode;
+					size++;
+				}
 			} else {
-				if(node.element > lastNode.element) {
-					lastNode.right = insertNode(node, lastNode.right);
+				if(newNode.element > lastNode.element) {
+					if(lastNode.right) {
+						insertNode(lastNode.right, newNode);
+					} else {
+						lastNode.right = newNode;
+						size++;
+					}
 				}
 				return null;
 			}
 		} else {
-			lastNode = node;
+			lastNode = newNode;
 		}
 		return lastNode;
 	};
 
-// 	void tree::insert(cliente* newData, treeNode** nodePtr)
-// {
-//         if(!*nodePtr)
-//         {
-//                 *nodePtr = new treeNode(newData);
-//                 ShowMessage("Inserido com sucesso!");
-//         }
-//         else
-//                 if(newData->getNome() < (*nodePtr)->data->getNome())
-//                         insert(newData,&((*nodePtr)->leftPtr));
-//                 else
-//                         if(newData->getNome() > ((*nodePtr)->data->getNome()))
-//                                 insert(newData, &((*nodePtr)->rightPtr));
-//                         else
-//                                 ShowMessage("Já existe: "+(*nodePtr)->data->getNome());
-// }
-
-	// var insertNode = function(node, parentNode) {
-	// 	if(root === null) {
-	// 		root = node;
-	// 		return root;
-	// 	}
-
-	// 	if(parentNode === null) {
-	// 		parentNode = root;
-	// 	}
-
-	// 	if(parentNode.left === null) {
-	// 		return (parentNode.left = node);
-	// 	} else if(parentNode.right === null) {
-	// 		return (parentNode.right = node);
-	// 	}
-
-	// 	if(node.element > parentNode.left.element) {
-	// 		return insertNode(node, parentNode.left);
-	// 	} else {
-	// 		return insertNode(node, parentNode.right);
-	// 	}
-	// };
-
 	var navigatePreOrder = function(lastNode, callback) {
-		if(lastNode !== null) {
+		if(lastNode) {
 			navigatePreOrder(lastNode.left, callback);
 			navigatePreOrder(lastNode.right, callback);
 			callback(lastNode.element);
 		}
 	};
 
-	var self = {
+	var navigateInOrder = function(lastNode, callback) {
+		if(lastNode) {
+			navigateInOrder(lastNode.left, callback);
+			callback(lastNode.element);
+			navigateInOrder(lastNode.right, callback);
+		}
+	};
+
+	var navigatePosOrder = function(lastNode, callback) {
+		if(lastNode) {
+			callback(lastNode.element);
+			navigateInOrder(lastNode.left, callback);
+			navigateInOrder(lastNode.right, callback);
+		}
+	};
+
+	return {
 		insert: function(element) {
-			var node = createNode(element);
+			var node = nodeBuilder.create({
+				element: element,
+				left: null,
+				right: null
+			});
+
 			if(root === null) {
 				root = node;
 			} else {
-				insertNode(node, root);
+				insertNode(root, node);
 			}
-
 		},
 		remove: function(element) {
 
 		},
 		preOrder: function(callback) {
-			if(root != null) {
-				navigatePreOrder(root, callback);
-			}
+			navigatePreOrder(root, callback);
 		},
 		inOrder: function(callback) {
-
+			navigateInOrder(root, callback);
 		},
 		posOrder: function(callback) {
-
+			navigatePosOrder(root, callback);
 		},
 		isEmpty: function() {
-
+			return root === null;
 		},
 		size: function() {
 			return size;
@@ -125,40 +107,24 @@ function BinaryTree() {
 			if(self.isEmpty()) {
 				return 'empty binary_tree';
 			}
-		},
-		root: function() {
-			return root;
 		}
 	};
-
-	return self;
 }
 
 module.exports = binaryTree;
 
 var tree = binaryTree.create();
-tree.insert(2);
-tree.insert(7);
 tree.insert(5);
-tree.insert(1);
-tree.insert(6);
-// tree.insert(9);
-// tree.insert(3);
-// tree.insert(11);
-// tree.insert(4);
+tree.insert(12);
+tree.insert(10);
+tree.insert(15);
+tree.insert(20);
+tree.insert(25);
+tree.insert(8);
+tree.insert(3);
 
-// tree.insert(2);
-// tree.insert(7);
-// tree.insert(5);
-// tree.insert(2);
-// tree.insert(6);
-// tree.insert(9);
-// tree.insert(5);
-// tree.insert(11);
-// tree.insert(4);
+console.log('size', tree.size());
 
-tree.preOrder(function(elem) {
+tree.posOrder(function(elem) {
 	console.log(elem);
 });
-
-// console.log(tree.root());
